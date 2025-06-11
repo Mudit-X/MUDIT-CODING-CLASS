fc153809aa4ca3fe279a928e238a7fe4c00f26e6
Mudit-X <kmudit262@gmail.com>
2025-06-11 13:44:01 +0800

    L03

---
+++ L03/L03-Rule Based Chatbot.py
@@ -0,0 +1,46 @@
+import re
+import random
+from colorama import Fore, init
+
+#Initialize colorama
+init(autoreset=True)
+
+# Data structures for travel recommendations and jokes
+destinations = {
+    "beaches": ["Bali", "Maldives", "Phuket"],
+    "mountains": ["Swiss Alps", "Rocky Mountains", "Himalayas"],
+    #add cities
+}
+
+jokes = [
+    "Why don't programmers like nature? Too many bugs!",
+    "Why did the computer go to the doctor? Because it had a virus!",
+    "Why do travelers always feel warm? Because of all their hot spots!"
+]
+
+# Function to greet the user
+def greet_user():
+    print(Fore.CYAN + "Hello I'm TravelBot, your virtual travel following:")
+    
+
+# Function to show help options
+
+    
+
+# Function to process user input
+
+
+# Function to provide travel recommendations
+
+    # Show the help options again
+
+# Function to offer packing tips
+
+
+# Function to tell a joke
+
+
+# Main chat function
+
+
+# Start the chatbot
